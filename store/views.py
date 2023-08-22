bfec44cf059bf19ed61fcd1c862f228b259e453c
masumeazee <masume.azee@gmail.com>
2023-08-23 00:58:58 +0330

    update file

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.http import request
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
+import datetime
 import json
 
 
@@ -14,7 +15,7 @@ def store(requset):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0 , 'shipping' : False}
         cartItems = order['get_cart_items']
 
     products = Product.objects.all()
@@ -30,7 +31,7 @@ def cart(request):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0 , 'shipping' : False}
         cartItems = order['get_cart_items']
     context = {'items': items, 'order': order,'cartItems':cartItems}
     return render(request, 'store/cart.html', context)
@@ -45,7 +46,7 @@ def checkout(requset):
         cartItems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0 , 'shipping' : False}
         cartItems = order['get_cart_items']
 
     context = {'items': items, 'order': order,'cartItems':cartItems}
@@ -78,6 +79,32 @@ def updateItem(request):
     else:
         return JsonResponse('No data received', safe=False)
     
-    
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+
+            )    
+    else:
+        print('user is not logged in .. ')    
+    return JsonResponse('payment complete!', safe= False)    
     
     
